c1aa1362ef95bd24629c56ff24cea2e67101fb06
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-08-29 20:35:44 -0700

    Square1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The module of Square"""
+
+class Square:
+    """Square Definition"""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size

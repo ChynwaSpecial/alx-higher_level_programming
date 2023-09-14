46551bb95f736187ddb7ee9f481660b0ea0fef87
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-09-13 20:09:05 -0700

    Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""Defines a class Student"""
+"""Defines a class Student."""
+
 
 class Student:
-    """Represent a student"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialization of the Student"""
+        """Initialize a new Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student"""
-          if (type(attrs) == list and
+        if (type(attrs) == list and
                 all(type(item) == str for item in attrs)):
             return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
         return self.__dict__
 
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student"""
+        for i, j in json.items():
+            setattr(self, i, j)

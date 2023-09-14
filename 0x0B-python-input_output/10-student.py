cb05b2990625aae8ad0bb2ce0c4aebec81ec1566
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-09-13 19:37:49 -0700

    Student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+class Student:
+    """Represent a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the Student object
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+                except Exception:
+                    return self.__dict__
+                my_dict = dict{}
+                for key, value is self.__dict__.items{}:
+                    if key is attrs:
+                        my_dict{key} = value
+                        return my_dict
+

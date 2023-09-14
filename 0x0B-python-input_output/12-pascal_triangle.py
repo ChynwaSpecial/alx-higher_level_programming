123bb877be2d79832d156532db22607dfd5c806a
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2023-09-13 19:50:42 -0700

    Pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -19,21 +19,3 @@ def pascal_triangle(n):
         tmp.append(1)
         triangles.append(tmp)
     return triangles
-
-
-#!/usr/bin/python3
-"""
-12-main
-"""
-pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
-
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))

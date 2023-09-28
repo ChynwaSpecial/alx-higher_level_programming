64d5d7f7b6d342358a016e8b4900430f50bc0abf
ChynwaSpecial <125501641+ChynwaSpecial@users.noreply.github.com>
2023-09-28 07:30:05 +0100

    Delete 0x08-python-more_classes directory

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-"""Define a Rectangle Class"""
-
-class Rectangle:
-    """Rectangle Representation"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-More illustration of Python Classes and Objects

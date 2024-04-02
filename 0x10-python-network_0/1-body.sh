fd68b5aeee3a5662b67c1b74a9e6aa963db9d7e5
ChynwaSpecial <chinenyeifeoma26@gmail.com>
2024-04-02 02:58:57 +0000

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# Get the URL from the argument
-url=$1
-
 # Send a GET request to the URL and display the body if the status code is 200
-response=$(curl -s -w "%{http_code}" -o temp.txt "$url")
-status_code=$(tail -n1 temp.txt)
-if [ "$status_code" = "200" ]; then
-    cat temp.txt
-else
-    echo "Status code: $status_code"
+response=$(curl -s -w "%{http_code}" -o temp.txt "$url")status_code=$(tail -n1 temp.txt)
